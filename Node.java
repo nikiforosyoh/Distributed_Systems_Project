1ac3205fa353e42797373db5e16440447daeeda9
Εlena Lagiou <39525687+hellag@users.noreply.github.com>
2020-03-09 15:59:12 +0200

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,18 @@
+
+import com.sun.corba.se.pept.broker.Broker;
+
+import java.util.ArrayList;
+import java.util.List;
+//or import java.util.*;
+public interface Node {
+    //fields
+    List<Broker> brokers=new ArrayList<Broker>();
+
+    //methods
+    public void init(int x);
+    public List<Broker> getBrokers();
+    public void connect();
+    public void discconnect();
+    public void updateNodes();
+
+}
