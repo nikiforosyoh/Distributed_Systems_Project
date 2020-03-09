b77ef488b902c877173bef0b9e471347ba9ce8e2
Εlena Lagiou <39525687+hellag@users.noreply.github.com>
2020-03-09 16:21:45 +0200

    Add files via upload

---
+++ ArtistName.java
@@ -0,0 +1,3 @@
+public class ArtistName {
+    protected String artistName;
+}
---
+++ MusicFile.java
@@ -0,0 +1,8 @@
+public class MusicFile {
+    //fields
+    protected String trackName;
+    protected String artistName;
+    protected String albumInfo;
+    protected String genre;
+    protected byte musicFileExtract;//byte [] ???
+}
