572f3497f951c0123984614c0f5f2a525941b630
Εlena Lagiou <39525687+hellag@users.noreply.github.com>
2020-03-09 17:01:50 +0200

    Add files via upload

--- ArtistName.java
+++ ArtistName.java
@@ -1,3 +1,7 @@
 public class ArtistName {
     protected String artistName;
+
+    public ArtistName(String artistName){
+        this.artistName=artistName;
+    }
 }
--- MusicFile.java
+++ MusicFile.java
@@ -1,8 +1,21 @@
+import java.util.ArrayList;
+
 public class MusicFile {
     //fields
     protected String trackName;
-    protected String artistName;
+    //protected String artistName;
+    protected ArtistName artistName;
     protected String albumInfo;
     protected String genre;
-    protected byte musicFileExtract;//byte [] ???
+    protected byte[] musicFileExtract;//byte [] ???
+
+    public MusicFile(String trackName, ArtistName artistName, String albumInfo, String genre, byte[] musicFileExtract){
+        this.trackName=trackName;
+        this.artistName=artistName;//??
+        this.albumInfo=albumInfo;
+        this.genre=genre;
+        for(int i=0;i<musicFileExtract.length;i++){
+            this.musicFileExtract[i]=musicFileExtract[i];
+        }
+    }
 }
