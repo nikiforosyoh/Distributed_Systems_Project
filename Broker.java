47214e8b2b5ae78b07e6fe6425b494bdf86c9628
K Mih <kmihalakakou@gmail.com>
2020-03-19 20:45:48 +0200

    Update Broker.java

--- Broker.java
+++ Broker.java
@@ -6,10 +6,22 @@ public class Broker extends Node{
     List<Consumer> registerdUsers = new ArrayList<Consumer>();
     List<Publisher> registerdPublishers = new ArrayList<Publisher>();
 
-    public void calculateKeys(){}
-    public Publisher acceptConection(Publisher p){ return null; }
-    public Consumer acceptConection(Consumer c){return null;}
-    public void notifyPublisher(String s){}
-    public void pull(ArtistName a){}
+    public void calculateKeys(){
+    
+    }
+    public Publisher acceptConection(Publisher p){ 
+        
+        return null;
+    }
+    public Consumer acceptConection(Consumer c){
+        
+        return null;
+    }
+    public void notifyPublisher(String s){
+    
+    }
+    public void pull(ArtistName a){
+    
+    }
     
 }
