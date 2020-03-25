d89b1b7d0544be91ede9784e74a0d2247400f567
yoh <43173037+nikiforosyoh@users.noreply.github.com>
2020-03-25 18:30:57 +0200

    Update ReadMp3Files.java

--- ReadMp3Files.java
+++ ReadMp3Files.java
@@ -1,4 +1,3 @@
-
 import com.mpatric.mp3agic.InvalidDataException;
 import com.mpatric.mp3agic.Mp3File;
 import com.mpatric.mp3agic.NotSupportedException;
@@ -16,49 +15,39 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class ReadMp3Files
+public class readMp3Files
 {
-    ArrayList<String> artistsname=new ArrayList<>();
-    ArrayList<String> ca=new ArrayList<>();
-    public ArrayList<String> Read() throws UnsupportedTagException, InvalidDataException, IOException
+    public static void main(String args[]) throws UnsupportedTagException, InvalidDataException, IOException
     {
-        String path = "C:\\Hellag\\AUEB\\2019_2020(final)\\spring\\distributed_systems\\projects\\dataset1\\dataset1\\Test";
+        String path = "C:\\Users\\DELL\\Desktop\\Distributed Systems\\Songs";
 
-        try (Stream<Path> walk = Files.walk(Paths.get(path))) {
-            List<String> result = walk.filter(Files::isRegularFile).map(Path::toString).collect(Collectors.toList());
+        Stream<Path> walk = Files.walk(Paths.get(path));
+        List<String> result = walk.filter(Files::isRegularFile).map(Path::toString).collect(Collectors.toList());
+        int sc = 0;
+        for(int i = 0 ; i <result.size() ; i++)
+        {
+            File file = new File(result.get(i));
+            Mp3File mp3file = new Mp3File(file);
 
-            for(int i = 0 ; i <result.size() ; i++)
+
+            if (mp3file.hasId3v2Tag())
             {
-                File file = new File(result.get(i));
-                Mp3File mp3file = new Mp3File(file);
-
-                if (mp3file.hasId3v1Tag())
-                {
-                    ID3v1 id3v1Tag = mp3file.getId3v1Tag();
-                    System.out.println("Song Name: " + file.getName().substring(0, file.getName().length()-4));
-                    System.out.println("Artist: " + id3v1Tag.getArtist() + "\n");
-
-                }
-
-                if (mp3file.hasId3v2Tag())
-                {
-                    ID3v2 id3v2Tag = mp3file.getId3v2Tag();
-                    System.out.println("Song Name: " + file.getName().substring(0, file.getName().length()-4));
-                    System.out.println("Artist: " + id3v2Tag.getArtist() + "\n");
-
-                }else {
-                    System.out.println("no");
-                }
-
+                ID3v2 id3v2Tag = mp3file.getId3v2Tag();
+                System.out.println("This is id3v2");
+                System.out.println("Song Name: " + file.getName().substring(0, file.getName().length()-4));
+                System.out.println("Artist: " + id3v2Tag.getArtist());
+                System.out.println("Album: " + id3v2Tag.getAlbum());
+                System.out.println("Genre: " + id3v2Tag.getGenre() + "\n");
             }
+            sc++;
         }
 
-        ca= CountArtists(path);
-        System.out.println(ca);
-        return ca;
+        int ca = CountArtists(path);
+        System.out.println("Artist count: " + ca);
+        System.out.println("Song count: " + sc);
     }
-//tropopoihsh methodou
-    public static ArrayList<String> CountArtists(String pathToMp3Files)
+
+    public static int CountArtists(String pathToMp3Files)
     {
         int ArtistCount = 0;
         ArrayList<String> Artists = new ArrayList<String>();
@@ -75,7 +64,6 @@ public class ReadMp3Files
                 if (mp3file.hasId3v1Tag())
                 {
                     artist = mp3file.getId3v1Tag().getArtist();
-
                 }
                 else if (mp3file.hasId3v2Tag())
                 {
@@ -87,7 +75,6 @@ public class ReadMp3Files
                     Artists.add(artist);
                     ArtistCount++;
                 }
-
             }
         }
         catch (IOException e)
@@ -103,7 +90,7 @@ public class ReadMp3Files
             e.printStackTrace();
         }
 
-        return Artists;
+        return ArtistCount;
     }
 
 }
