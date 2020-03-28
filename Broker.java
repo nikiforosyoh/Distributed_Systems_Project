58feba0a788b8627c5471740986978ec4bf5d095
K Mih <kmihalakakou@gmail.com>
2020-03-28 14:08:51 +0200

    Update Broker.java

--- Broker.java
+++ Broker.java
@@ -3,17 +3,22 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.net.*;
+//import java. net. InetAddress;
 
 public class Broker extends Node{
-
+    //String  mainIp ="192.168.0.1";//menei statheri
     String ipBroker="127.0.0.1";
-    int ConsumersPort=5000;
-    int PublishersPort=5001;
 
+    int ConsumersPort=5004;
+    int PublishersPort=5005;
+    int key=3;
+    //int BrokerPort = 5000;//meni statheri
 
     ServerSocket ConsumerServer = null;
     ServerSocket PublisherServer=null;
-    Socket connect = null;
+    //ServerSocket BrokerServer = null;
+
+   // Socket connect = null; //to dilwnw katw
     private DataInputStream in = null;
     List<Consumer> registerdUsers = new ArrayList<Consumer>();
     List<Publisher> registerdPublishers = new ArrayList<Publisher>();
@@ -37,12 +42,13 @@ public class Broker extends Node{
 
     public Broker(){}
     public Broker(Socket socket){
-        this.connect=socket;
+        //this.connect=socket;
     }
 
 
     public void openServer(){
-        createTxt(ConsumersPort,PublishersPort);
+
+       // createTxt(ConsumersPort,PublishersPort);
         try {
             ConsumerServer=new ServerSocket(ConsumersPort);
             System.out.println("Broker> waiting for connection...");
@@ -58,12 +64,12 @@ public class Broker extends Node{
                         while(true){
                             Socket socket2= PublisherServer.accept();
                             System.out.println("Publisher accepted! --> " + socket2.getInetAddress().getHostAddress());
-                            PublisherThread pt=new PublisherThread(socket2);
+                            PublisherThread pt=new PublisherThread(socket2,key);
                             pt.start();
                         }
 
                     }
-                     catch (IOException e) {
+                    catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
@@ -71,9 +77,9 @@ public class Broker extends Node{
             t1.start();
 
             while(true) {
-                connect = ConsumerServer.accept();
-                System.out.println("Consumer accepted! --> " + connect.getInetAddress().getHostAddress());
-                ConsumerThread ct=new ConsumerThread(connect);
+                Socket socket1 = ConsumerServer.accept();//to dilwnw edw anti gia panw
+                System.out.println("Consumer accepted! --> " + socket1.getInetAddress().getHostAddress());
+                ConsumerThread ct=new ConsumerThread(socket1);
                 ct.start();
             }
 
@@ -86,6 +92,7 @@ public class Broker extends Node{
     public Consumer acceptConection(Consumer c){return null; }
     public void notifyPublisher(String s){}
     public void pull(ArtistName a){}
+
 
 
     public static void main(String[] args) throws IOException {
