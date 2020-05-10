6260ddfa1d1c99b1daffba45683ed2fbf065453a
Εlena Lagiou <39525687+hellag@users.noreply.github.com>
2020-05-10 23:18:43 +0300

    Add files via upload

---
+++ android/java/com/example/spotifypro/MusicPlay.java
@@ -0,0 +1,14 @@
+package com.example.spotifypro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MusicPlay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_play);
+    }
+}
